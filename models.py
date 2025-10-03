be6a66ad2385c5592224184c417454989c0da2a4
Your Name <you@example.com>
2025-10-03 10:29:42 -0700

    Update database URL for Render

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://postgres:postgres@localhost/todo_api')
+engine = create_engine('postgresql://todo_api_db_bsxu_user:6OmhOyBbaj6ZSGXj87bTO8CGO72hUHgp@dpg-d3g23ipr0fns73dmven0-a:5432/todo_api_db_bsxu')
 Base = declarative_base()
 
 class Task(Base):
